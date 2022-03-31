7b36a110a96671aaf8c53eb60f431eac8bfcdcf6
José Victor Teixeira <jose.v.teixeira@kroton.com.br>
2022-03-31 06:54:47 -0300

    Ajuste na conexão do banco de dados para conectar ao DB, criação dos models de Cidade e Pessoa e projeto para versionamento do DB

---
+++ src/Example.Application/ExampleService/Models/Request/PersonRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Application.ExampleService.Models.Request
+{
+    public record PersonRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public int CityId { get; set; }
+        public int Age { get; set; }
+    }
+}
